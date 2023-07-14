6f01a7f8910f21d5040bf8c00db1780bc829d742
Ade131 <aidankelly202@gmail.com>
2023-07-14 12:24:06 +0100

    Initial Commit

---
+++ Four Six/Four_SixApp.swift
@@ -0,0 +1,17 @@
+//
+//  Four_SixApp.swift
+//  Four Six
+//
+//  Created by Aidan Kelly on 14/07/2023.
+//
+
+import SwiftUI
+
+@main
+struct Four_SixApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
