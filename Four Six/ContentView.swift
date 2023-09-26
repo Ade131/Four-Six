b0fb49f2764c95a82fd7fc174e88c8839cce9bee
Ade131 <aidankelly202@gmail.com>
2023-09-26 14:18:33 +0100

    add new selector view for edit amount

--- Four Six/ContentView.swift
+++ Four Six/ContentView.swift
@@ -15,8 +15,11 @@ struct ContentView: View {
     @State private var showOptions = false
     @State private var navigateToBrew = false
     @State private var showPicker = false
+    @State private var pickerHeight: CGFloat = 0
+    @State private var selectedPickerIndex = 0
+    @State private var editing = false
     var pickerOptions: [Int] {
-        Array(stride(from: 100, to: 501, by: 10))
+        Array(stride(from: 200, to: 601, by: 10))
     }
     
     
@@ -36,77 +39,88 @@ struct ContentView: View {
                         .font(.headline)
                         .padding(.top)
                     
+                    
                     VStack(spacing: 3) {
                         Text("\(waterInput) ml")
                             .font(.title)
                             .fontWeight(.bold)
                         
-                        Text("Edit")
-                            .foregroundColor(Color.linkColour)
-                            .onTapGesture {
-                                showPicker = true
+                        if pickerHeight == 0 {
+                            Button("Edit") {
+                                withAnimation {
+                                    pickerHeight = 150
+                                }
                             }
-                            .actionSheet(isPresented: $showPicker) {
-                                ActionSheet(title: Text("Select water amount (ml)"),
-                                            buttons: pickerOptions.map { option in
-                                        .default(Text("\(option)")) {
-                                            waterInput = "\(option)"
-                                        }
-                                })
+                        } else {
+                            Button("Done") {
+                                withAnimation {
+                                    pickerHeight = 0
+                                }
                             }
-                    }
-                    
-                    VStack(spacing: 3) {
-                        Text("Prepare")
-                            .font(.headline)
-                        Text(" \(Int((Double(waterInput) ?? 250) / Double(coffeeModel.ratio)))g")
-                            .font(.title)
-                            .fontWeight(.bold)
-                        Text("of ground coffee")
-                            .font(.headline)
-                        
-                        NavigationLink("", destination: BrewingView(), isActive: $navigateToBrew)
-                            .hidden()
-                    }
-                    .padding(.top)
-                    
-                    Button("Options") {
-                        showOptions.toggle()
-                    }
-                    .sheet(isPresented: $showOptions) {
-                        OptionsView()
-                    }
-                    .buttonStyle(OptionsButton())
-                    .padding()
-                    
-                    Button("Start Brewing") {
-                        if let waterWeight = Double(waterInput) {
-                            coffeeModel.updateWaterWeight(weight: waterWeight)
-                            coffeeModel.calculatePours()
-                            navigateToBrew = true
                         }
+                        
+                        Picker("Select water amount (ml)", selection: $waterInput) {
+                            ForEach(pickerOptions, id: \.self) { option in
+                                Text("\(option)").tag("\(option)")
+                            }
+                        }
+                        .labelsHidden()
+                        .pickerStyle(WheelPickerStyle())
+                        .frame(height: pickerHeight)
+                        .clipped()
                     }
-                    .buttonStyle(StartButton())
-                    
-                    Spacer()
-                }
-                .toolbar {
-                    ToolbarItem(placement: .navigationBarTrailing) {
-                        NavigationLink(destination: SettingsView().environmentObject(coffeeModel)) {
-                            Image(systemName: "gearshape.fill")
-                                .foregroundColor(Color.iconColour)
+                        
+                        VStack(spacing: 3) {
+                            Text("Prepare")
+                                .font(.headline)
+                            Text(" \(Int((Double(waterInput) ?? 250) / Double(coffeeModel.ratio)))g")
+                                .font(.title)
+                                .fontWeight(.bold)
+                            Text("of ground coffee")
+                                .font(.headline)
+                            
+                            NavigationLink("", destination: BrewingView(), isActive: $navigateToBrew)
+                                .hidden()
+                        }
+                        .padding(.top)
+                        
+                        Button("Options") {
+                            showOptions.toggle()
+                        }
+                        .sheet(isPresented: $showOptions) {
+                            OptionsView()
+                        }
+                        .buttonStyle(OptionsButton())
+                        .padding()
+                        
+                        Button("Start Brewing") {
+                            if let waterWeight = Double(waterInput) {
+                                coffeeModel.updateWaterWeight(weight: waterWeight)
+                                coffeeModel.calculatePours()
+                                navigateToBrew = true
+                            }
+                        }
+                        .buttonStyle(StartButton())
+                        
+                        Spacer()
+                    }
+                    .toolbar {
+                        ToolbarItem(placement: .navigationBarTrailing) {
+                            NavigationLink(destination: SettingsView().environmentObject(coffeeModel)) {
+                                Image(systemName: "gearshape.fill")
+                                    .foregroundColor(Color.iconColour)
+                            }
                         }
                     }
                 }
             }
         }
     }
-}
-
-struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
-            .environmentObject(CoffeeBrewingModel())
+    
+    struct ContentView_Previews: PreviewProvider {
+        static var previews: some View {
+            ContentView()
+                .environmentObject(CoffeeBrewingModel())
+        }
     }
-}
-
+    
